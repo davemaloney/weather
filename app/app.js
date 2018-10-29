5fef6a9c7dd0d1b1c2c32bb686d1a946dedcb324
Dave Maloney <david.s.maloney@gmail.com>
2018-10-28 22:19:49 -0400

    Switch style base to Bootstrap; Local styling will be done in Less

--- app/app.js
+++ app/app.js
@@ -14,7 +14,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router/immutable';
 import history from 'utils/history';
-import 'sanitize.css/sanitize.css';
+// import 'sanitize.css/sanitize.css';
 
 // Import root app
 import App from 'containers/App';
