7dce148cd7d29aa1e1c754e60aa826b413ad9603
Dave Maloney <david.s.maloney@gmail.com>
2018-10-30 15:56:46 -0400

    Add tests for forecast container.

--- app/containers/ForecastContainer/tests/actions.test.js
+++ app/containers/ForecastContainer/tests/actions.test.js
@@ -1,13 +1,47 @@
-import { defaultAction } from '../actions';
-import { DEFAULT_ACTION } from '../constants';
+import {
+  getByCoords,
+  getWeather,
+  getWeatherFail,
+  getWeatherSuccess,
+} from '../actions';
+import {
+  GET_BY_COORDS,
+  GET_WEATHER,
+  GET_WEATHER_FAIL,
+  GET_WEATHER_SUCCESS,
+} from '../constants';
 
 describe('ForecastContainer actions', () => {
-  describe('Default Action', () => {
-    it('has a type of DEFAULT_ACTION', () => {
+  describe('Get Weather', () => {
+    it('has a type of GET_WEATHER', () => {
       const expected = {
-        type: DEFAULT_ACTION,
+        type: GET_WEATHER,
       };
-      expect(defaultAction()).toEqual(expected);
+      expect(getWeather()).toEqual(expected);
+    });
+  });
+  describe('Get By Coordinates', () => {
+    it('has a type of GET_BY_COORDS', () => {
+      const expected = {
+        type: GET_BY_COORDS,
+      };
+      expect(getByCoords()).toEqual(expected);
+    });
+  });
+  describe('Get Weather Success', () => {
+    it('has a type of GET_WEATHER_SUCCESS', () => {
+      const expected = {
+        type: GET_WEATHER_SUCCESS,
+      };
+      expect(getWeatherSuccess()).toEqual(expected);
+    });
+  });
+  describe('Get Weather', () => {
+    it('has a type of GET_WEATHER_FAIL', () => {
+      const expected = {
+        type: GET_WEATHER_FAIL,
+      };
+      expect(getWeatherFail()).toEqual(expected);
     });
   });
 });
