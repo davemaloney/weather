fcde6553ea29f2fd764868342c9c697bbe5712f1
Dave Maloney <david.s.maloney@gmail.com>
2018-10-30 15:58:06 -0400

    Customize manifest

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -121,9 +121,9 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'Weather Forecast',
+      short_name: 'Weather',
+      description: 'My React Boilerplate-based weather forecast project!',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
